714d2f13cf93fd53da4a90f58de999a349805e46
Herlin R. Espinosa G. <herlin25@gmail.com>
2013-11-08 07:43:06 -0500

    Visual Change

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -24,6 +24,7 @@ if ($sf_user->isAuthenticated()) {
     $Username = sfContext::getInstance()->getUser()->getUsername();
     $CompleteName = sfContext::getInstance()->getUser()->getAttribute('CompleteName');
 }
+ECHO "Hola...";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
