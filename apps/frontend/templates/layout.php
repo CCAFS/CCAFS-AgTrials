6e28fb9c951dc6290d36fbaf500714117d16662f
hrespinosa <hrespinosa@CIATCO00135.CGIARAD.ORG>
2014-07-15 16:20:30 -0500

    Actualizacion Modulos

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
+//HERLIN R. ESPINOSA G.
+//herlin25@gmail.com
 require_once '../lib/funtions/funtion.php';
 $pop = sfContext::getInstance()->getRequest()->getParameterHolder()->get('pop');
 $module = sfContext::getInstance()->getRequest()->getParameterHolder()->get('module');
